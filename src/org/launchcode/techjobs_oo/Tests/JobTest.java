5836f7e050744e0e0aae0ef0829fe02cfe172259
Kristopher Johnson <krazyk786@gmail.com>
2019-12-01 17:48:01 -0600

    resolved library issues and setup JobTest

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,8 +1,22 @@
 package org.launchcode.techjobs_oo.Tests;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.Job;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class JobTest {
+    Job job1;
+    Job job2;
+    @Before
+    public void setup(){
+        job1 = new Job();
+        job2 = new Job();
+    }
+
     @Test
     public void testSettingJobId(){
-
+        assertTrue(job2.getId() == job1.getId() + 1);
     }
 }
